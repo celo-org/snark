7e03d413fb75e69f7b94163b9112665de69c592a
Nicholas Ward <npward@berkeley.edu>
2020-10-12 10:06:44 -0700

    Update snark/src/lib.rs
    Co-authored-by: Pratyush Mishra <pratyushmishra@berkeley.edu>

--- snark/src/lib.rs
+++ snark/src/lib.rs
@@ -1,5 +1,15 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+#![deny(
+    warnings,
+    unused,
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms,
+    missing_docs,
+)]
+#![forbid(unsafe_code)]
+
 use ark_ff::{PrimeField, ToBytes};
 use core::fmt::Debug;
 use ark_relations::r1cs::ConstraintSynthesizer;
